59741e3f304b38cbc99d943c4cd27e017d9eff1e
Your Name <royshalom911@gmail.com>
2020-07-29 02:18:30 +0300

    after feedback

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,11 @@
-//didnt understood why i should make it in a seperated Class if i can use it as a regular function **(תאריך תפוגה (מחלקה נפרדת))**
+function returnHigherPrice(pro1, pro2) {
+    if (pro1.price > pro2.price) {
+        return (`${pro1.name} costs more than the ${pro2.name}`);
+    }
+    else {
+        return (`${pro2.name} costs more than the ${pro1.name}`);
+    }
+}
 function randomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
@@ -25,14 +32,6 @@ class Product {
         return (this.price / this.weight);
     }
     //higher price
-    returnHigherPrice(pro1, pro2) {
-        if (pro1.price > pro2.price) {
-            return (`${pro1.name} costs more than the ${pro2.name}`);
-        }
-        else {
-            return (`${pro2.name} costs more than the ${pro1.name}`);
-        }
-    }
     // exp date 2 products
     expirationDate(pro1, pro2) {
         if (pro1.date > pro2.date) {
@@ -55,21 +54,21 @@ class Product {
     printProductToTable(product) {
         const tBody = document.getElementById("tbody");
         const tr = document.createElement("tr");
-        const tdName = document.createElement("td");
-        tdName.innerText = product.name;
-        const tdPrice = document.createElement("td");
+        const tdName = _getTd();
+        const tdPrice = _getTd();
+        const tdIsKosher = _getTd();
+        const tdWeight = _getTd();
+        const tdManufacturer = _getTd();
+        const priceForKiloT = _getTd();
+        const dateProduct = _getTd();
+        const isProductExp = _getTd();
         tdPrice.innerText = product.price;
-        const tdWeight = document.createElement("td");
+        tdName.innerText = product.name;
         tdWeight.innerText = product.weight;
-        const tdIsKosher = document.createElement("td");
         tdIsKosher.innerText = product.isKosher;
-        const tdManufacturer = document.createElement("td");
         tdManufacturer.innerText = product.manufacturer;
-        const priceForKiloT = document.createElement("td");
         priceForKiloT.innerText = product.priceForKilo();
-        const dateProduct = document.createElement("td");
         dateProduct.innerText = product.date;
-        const isProductExp = document.createElement("td");
         isProductExp.innerText = product.isExpired();
         tr.append(tdName);
         tr.append(tdPrice);
@@ -83,16 +82,30 @@ class Product {
     }
 }
 const container = document.getElementById("contain");
+//built in products
 const product1 = new Product("onion", 5, 2, true, "Osem");
 const product2 = new Product("tomato", 3, 2, true, "Elite");
 const product3 = new Product("Snickers", 8, 2.5, false, "Usa");
 const product4 = new Product("Banana", 2, 1.6, true, "Elite");
-container.innerText += product1.returnHigherPrice(product1, product2);
+container.innerText += returnHigherPrice(product1, product2);
 container.innerHTML += "<br></br>";
-container.innerText += product1.returnHigherPrice(product2, product4);
+container.innerText += returnHigherPrice(product2, product4);
 container.innerHTML += "<br></br>";
 container.innerText += product1.expirationDate(product1, product2);
 product1.printProductToTable(product1);
-product1.printProductToTable(product2);
-product1.printProductToTable(product3);
-product1.printProductToTable(product4);
+product2.printProductToTable(product2);
+product3.printProductToTable(product3);
+product4.printProductToTable(product4);
+const btn = document.getElementById("btn");
+const nameInput = document.getElementById("name");
+const priceInput = document.getElementById("price");
+const weightInput = document.getElementById("weight");
+//  forgot to add inputs (didnt understood the assigment)
+btn.addEventListener("click", () => {
+    const addedProduct = new Product(nameInput.value, priceInput.value, weightInput.value, true, "International");
+    addedProduct.printProductToTable(addedProduct);
+});
+function _getTd() {
+    const tdName = document.createElement("td");
+    return tdName;
+}
